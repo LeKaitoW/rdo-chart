93a7709bb03ebe0607d4778a2ddbca56bb63c722
LeKaitoW <le.chat.noir.kaito@gmail.com>
2015-03-18 01:12:22 +0300

    SWT ten points test(lekaitow/rdo-chart#1)

--- jfreechart.test/src/SWTTenPoints.java
+++ jfreechart.test/src/SWTTenPoints.java
@@ -7,74 +7,70 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
+import org.jfree.data.Range;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 import org.jfree.experimental.chart.swt.ChartComposite;
 
-public class SWTTenPoints
-{
-    private static JFreeChart createChart(XYDataset dataset) {
+public class SWTTenPoints {
+	private static JFreeChart createChart(XYDataset dataset) {
 
-        JFreeChart chart = ChartFactory.createXYStepChart(
-        		"10 points chart",
-        		"Time", 
-        		"Value",
-        		dataset,
-        		PlotOrientation.VERTICAL,
-        		true,
-        		true,
-        		false
-        );
+		final JFreeChart chart = ChartFactory.createXYStepChart(
+				"10 points chart", 
+				"Time", 
+				"Value", 
+				dataset,
+				PlotOrientation.VERTICAL, 
+				true, 
+				true, 
+				false);
 
-        XYPlot plot = (XYPlot) chart.getPlot();
+		XYPlot plot = (XYPlot) chart.getPlot();
 
-        NumberAxis firstRangeAxis = new NumberAxis("Ten points");
-        firstRangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
-		firstRangeAxis.setAutoRangeIncludesZero(true);
+		final NumberAxis rangeAxis = new NumberAxis();
+		rangeAxis.setRangeWithMargins(10, 20);
+		plot.setRangeAxis(rangeAxis);
 
-		final NumberAxis DomainAxis = new NumberAxis();
-		DomainAxis.setStandardTickUnits(NumberAxis.createStandardTickUnits());
-		DomainAxis.setAutoRangeIncludesZero(true);
-		plot.setDomainAxis(DomainAxis);
-        
-        return chart;
+		final NumberAxis domainAxis = new NumberAxis();
+		domainAxis.setRangeWithMargins(dataset.getXValue(0, 0),
+				dataset.getXValue(0, 9));
+		plot.setDomainAxis(domainAxis);
 
-    }
-    
-    private static XYDataset createDataset() {
+		return chart;
+	}
 
-        XYSeriesCollection dataset1 = new XYSeriesCollection();
-        XYSeries series1 = new XYSeries("10 points");
-        dataset1.addSeries(series1);
-        
-        Double x = 0.0;
-        int num = 10;
-        for (int i = 0; i < num; i++)
-        {
-        	Integer value = (int) (i + 10*Math.random());
-        	series1.add(x, value);
-        	x = x + 1.0;
-        }
-        
-        return dataset1;
-    }
+	private static XYDataset createDataset() {
 
-    public static void main(String[] args) {
-        final JFreeChart chart = createChart(createDataset());
-        final Display display = new Display();
-        Shell shell = new Shell(display);
-        shell.setSize(600, 300);
-        shell.setLayout(new FillLayout());
-        shell.setText("Ten points demo for jfreechart running with SWT");
-        ChartComposite frame = new ChartComposite(shell, SWT.NONE, chart, true);
-        frame.setDisplayToolTips(true);
-        frame.setHorizontalAxisTrace(false);
-        frame.setVerticalAxisTrace(false);
-        shell.open();
-        while (!shell.isDisposed()) {
-            if (!display.readAndDispatch())
-                display.sleep();
-        }
-    }
+		XYSeriesCollection dataset = new XYSeriesCollection();
+		XYSeries series1 = new XYSeries("10 points");
+		dataset.addSeries(series1);
+
+		Double x = 0.0;
+		final int numberOfPoints = 10;
+		for (int pointNumber = 0; pointNumber < numberOfPoints; pointNumber++) {
+			Integer y = (int) (numberOfPoints + 10 * Math.random());
+			series1.add(x, y);
+			x = x + 1.0;
+		}
+		return dataset;
+	}
+
+	public static void main(String[] args) {
+		final JFreeChart chart = createChart(createDataset());
+		final Display display = new Display();
+		Shell shell = new Shell(display);
+		shell.setSize(600, 300);
+		shell.setLayout(new FillLayout());
+		shell.setText("Ten points demo for jfreechart running with SWT");
+		ChartComposite frame = new ChartComposite(shell, SWT.NONE, chart, true);
+		frame.setDisplayToolTips(true);
+		frame.setHorizontalAxisTrace(false);
+		frame.setVerticalAxisTrace(false);
+		shell.open();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch())
+				display.sleep();
+		}
+	}
 }
